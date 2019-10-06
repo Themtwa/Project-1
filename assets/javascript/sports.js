ca5cb613b40521a95c2cfcb366dd0d8978aed74b
Matthew Tarbet <goatdaddy08@yahoo.com>
2019-10-06 12:42:28 -0600

    working weather api

--- assets/javascript/sports.js
+++ assets/javascript/sports.js
@@ -1,7 +1,9 @@
-var queryURL = "https://api.darksky.net/forecast/[cc202cd492093c30bf98858baf250d00]/[20],[20]"
+var queryURL = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/cc202cd492093c30bf98858baf250d00/42.3601,-71.0589"
+var lat = ""
+var long = ""
 $.ajax({
     url: queryURL,
     method: "GET",
-}).then(function(response){
+}).then(function (response) {
     console.log(response)
 })

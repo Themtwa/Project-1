9dde1e81d04465ba0c5e1e2f0d74cff8fc7d3e29
Jacob Beuchert <jacobbeuchert@gmail.com>
2019-10-06 21:43:54 -0600

    Adding HTML code to try and pull my information from the API data.

--- assets/javascript/sports.js
+++ assets/javascript/sports.js
@@ -1,5 +1,50 @@
-var queryURL = "https://api.sportradar.us/nfl/official/trial/v5/en/games/2019/REG/05/schedule.json?api_key=zjdgsqf346kgnng7vy4fa4ssx";
+var teams = [
+  "Arizona Cardinals",
+  "Atlanta Falcons",
+  "Baltimore Ravens",
+  "Buffalo Bills",
+  "Carolina Panthers",
+  "Chicago Bears",
+  "Cincinnati Bengals",
+  "Cleveland Browns",
+  "Dallas Cowboys",
+  "Denver Broncos",
+  "Detroit Lions",
+  "Green Bay Packers",
+  "Houston Texans",
+  "Indianapolis Colts",
+  "Jacksonville Jaguars",
+  "Kansas City Chiefs",
+  "Los Angeles Chargers",
+  "Los Angeles Rams",
+  "Miami Dolphins",
+  "Minnesota Vikings",
+  "New England Patriots",
+  "New Orleans Saints",
+  "New York Giants",
+  "New York Jets",
+  "Oakland Raiders",
+  "Philadelphia Eagles",
+  "Pittsburgh Steelers",
+  "San Francisco 49ers",
+  "Seattle Seahawks",
+  "Tampa Bay Buccaneers",
+  "Tennessee Titans",
+  "Washington Redskins",
+  ];
 
-  $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
-    console.log(response)
-  });
+  function displayTeamInfo(){
+
+    var team = $(this).attr("data-name");
+
+    var queryURL = "https://api.sportradar.us/nfl/official/trial/v5/en/games/2019/REG/schedule.json?/api_key=zjdgsqf346kgnng7vy4fa4ss";
+
+    $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
+      console.log(response)
+      
+    });
+    var teamDiv = $("<div class='team'>");
+
+    var 
+
+}

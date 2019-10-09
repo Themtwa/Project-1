12103a15269f54563877a9706e6ab0a7e9298205
Jacob Beuchert <jacobbeuchert@gmail.com>
2019-10-09 15:11:07 -0600

    Adding the code to create the table

--- assets/javascript/sports.js
+++ assets/javascript/sports.js
@@ -1,11 +1,11 @@
 
 
     var queryURL = "https://cors-anywhere.herokuapp.com/https://api.sportradar.us/nfl/official/trial/v5/en/games/2019/REG/05/schedule.json?api_key=zjdgsqf346kgnng7vy4fa4ss";
-
+    $(document).ready(function(){
     $.ajax({
-      url: queryURL, 
+      url: queryURL,
+      dataType: 'json',
       method: "GET" }).then(function(response) {
-        // console.log(response.week.games);
         for (var i = 0; i < response.week.games.length; i++) {
           var game = response.week.games[i];
           console.log(game.scheduled);
@@ -13,8 +13,14 @@
           console.log(game.away.name);
           console.log(game.scoring.home_points);
           console.log(game.scoring.away_points);
-
         }
+
+        $("#Schedule > tbody").append("<tr><td>" + game.scheduled + "</td><td>" + game.home.name + "</td><td>" + game.away.name + "</td><td>" + game.scoring.home_points + "</td><td>" + game.scoring.away_points + "</td></tr>");
+
+
+      
       });
+
+    });
 
      

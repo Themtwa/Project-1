a95fed614c7254f5800c153fed60beea57b827c8
Jacob Beuchert <jacobbeuchert@gmail.com>
2019-10-08 21:11:45 -0600

    Adding the for loop to add the scores

--- assets/javascript/sports.js
+++ assets/javascript/sports.js
@@ -1,43 +1,20 @@
-var Schedule = "";
-var Home = "";
-var Away = "";
-var Scoring = "";
-var HomePoints = "";
-var AwayPoints
-
-$("#add-team").ready(function() {
-
-  Schedule = $("#Schedule-input").val();
-  Home = $("#Home-input").val();
-  Away = $("#Away-input").val();
-  Scoring = $("#Scoring-input").val();
-  HomePoints = $("#HomePoints-input").val();
-  AwayPoints = $("#AwayPoints-input").val();
-
-  dataRef.ref().push({
-
-    Schedule: Schedule,
-    Home: Home,
-    Away: Away,
-    Scoring: Scoring,
-    HomePoints: HomePoints,
-    AwayPoints: AwayPoints,
-});
-
-console.log("Team Added")
 
 
-  function displayTeamInfo(){
-
-    var team = $(this).attr("data-name");
-
-    var queryURL = "https://api.sportradar.us/nfl/official/trial/v5/en/games/2019/REG/05/schedule.json?api_key=zjdgsqf346kgnng7vy4fa4ss";
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.sportradar.us/nfl/official/trial/v5/en/games/2019/REG/05/schedule.json?api_key=zjdgsqf346kgnng7vy4fa4ss";
 
     $.ajax({
       url: queryURL, 
-      method: "GET" }).done(function(response) {
-      console.log(response)
+      method: "GET" }).then(function(response) {
+        // console.log(response.week.games);
+        for (var i = 0; i < response.week.games.length; i++) {
+          var game = response.week.games[i];
+          console.log(game.scheduled);
+          console.log(game.home.name);
+          console.log(game.away.name);
+          console.log(game.scoring.home_points);
+          console.log(game.scoring.away_points);
 
-    });
+        }
+      });
 
-  }
+     
